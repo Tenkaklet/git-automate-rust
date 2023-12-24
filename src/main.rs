d5c9ce456d89751e7ff51847a4f2846ad2a296d3
Max Carlquist <tenkaklet@gmail.com>
2023-12-24 19:00:36 +0100

    chilly-push

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use std::process::{Command, exit};
 use names::Generator;
+use std::thread;
+use std::time::Duration;
+use std::{cmp::min, fmt::Write};
+
+use indicatif::{ProgressBar, ProgressState, ProgressStyle};
 
 fn update_commit_push() {
     let add_command = Command::new("git")
@@ -39,7 +44,7 @@ fn update_commit_push() {
         exit(1);
     }
 
-    
+
     println!("git push command success");
 
 }
@@ -51,5 +56,21 @@ fn name_genrator() -> String {
 
 
 fn main() {
+    let mut downloaded = 0;
+    let total_size = 231231231; // total size is the size of the repo
+    let pb = ProgressBar::new(total_size);
+    pb.set_style(ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})")
+        .unwrap()
+        .with_key("eta", |state: &ProgressState, w: &mut dyn Write| write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap())
+        .progress_chars("#>-"));
+    while downloaded < total_size {
+        let new = min(downloaded + 223211, total_size);
+        downloaded = new;
+        pb.set_position(new);
+        thread::sleep(Duration::from_millis(12));
+    }
+
+    pb.finish_with_message("downloaded");
+    
     update_commit_push();
 }
