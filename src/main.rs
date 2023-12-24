2e0b94d811e2dfaa0f0732f8e7dab2be73cd58c5
Max Carlquist <tenkaklet@gmail.com>
2023-12-24 18:16:43 +0100

    sweet-sort

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use std::process::{Command, exit};
+use names::Generator;
+
+fn update_commit_push() {
+    let add_command = Command::new("git")
+        .arg("add")
+        .arg("-A")
+        .output()
+        .expect("failed to execute git add command");
+    if !add_command.status.success() {
+        println!("git add command failed");
+        exit(1);
+    }
+
+    let commit_command = Command::new("git")
+        .arg("commit")
+        .arg("-m")
+        .arg(name_genrator())
+        .output()
+        .expect("failed to execute git commit command");
+
+    if !commit_command.status.success() {
+        println!("git commit command failed");
+        exit(1);
+    }
+
+    let push_command = Command::new("git")
+        .arg("push")
+        .arg("origin")
+        .arg("master")
+        .output()
+        .expect("failed to execute git push command");
+
+    if !push_command.status.success() {
+        println!("git push command failed");
+        exit(1);
+    }
+
+    println!("git push command success");
+
+}
+
+fn name_genrator() -> String {
+    let mut generator = Generator::default();
+    generator.next().unwrap()
+}
+
+
+fn main() {
+    update_commit_push();
+}
